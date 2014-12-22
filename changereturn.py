1bab45c447bc6928a36a34ded9be1e9811521140
Jeff Bahns <jeffbahns@gmail.com>
2014-12-22 11:48:40 -0800

    pennies don't work, but mostly done

--- changereturn.py
+++ changereturn.py
@@ -1,25 +1,22 @@
-# Change Return Program
+# python 2.7
+
 from collections import OrderedDict as od
-
-
-
 def change_return(cost, payment):
     cashback = payment - cost
     bills = cashback // 1
-    change = cashback - bills
+    change = (cashback % 1) * 100
     quarters = 0
-    coin_return ={  "Quarters"    : 0,
-                    "Dimes"       : 0,
-                    "Nickels"     : 0,
-                    "Pennies"     : 0}
-    coin_values = od([ ("Quarters", 0.25),
-                                ("Dimes"    , 0.10),
-                                ("Nickels"  , 0.05),
-                                ("Pennies"  , 0.01)])
+    coin_return ={       "Quarters" : 0,
+                         "Dimes" : 0,
+                         "Nickels" : 0,
+                         "Pennies" : 0}
+    coin_values = od([  ("Quarters", 25),
+                        ("Dimes" , 10),
+                        ("Nickels" , 5),
+                        ("Pennies" , 1)])
     for coin in coin_values:
-        print (coin, coin_values[coin], "  >>  ", change // coin_values[coin])
-    
-print (change_return(7.52, 10))
-
-print (50// 25)
-
+        quantity = change // coin_values[coin]
+        coin_return[coin] = quantity
+        change -= coin_values[coin] * quantity
+        print coin ,coin_return[coin]
+    return None
