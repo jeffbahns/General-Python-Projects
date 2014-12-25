d760351a14947d7abb8b1f7af61e871d96563143
Jeff Bahns <jeffbahns@gmail.com>
2014-12-25 09:49:51 -0800

    reduced size

--- checkifpalindrome.py
+++ checkifpalindrome.py
@@ -8,9 +8,5 @@ def check_if_palindrome(phrase):
     phrase = phrase.lower()
     stripped_phrase = re.sub("[^a-z]", "", phrase)
     reverse = stripped_phrase[::-1]
-    if reverse == stripped_phrase:
-        decision = True
-    else:
-        decision = False
-    return decision
+    return reverse == phrase
 
