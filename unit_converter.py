96b2de4ce9f3d6a7b32ce42c123b62474d177d5f
Jeff Bahns <jeffbahns@gmail.com>
2014-12-25 18:53:36 -0800

    Finished temperature conversions

--- unit_converter.py
+++ unit_converter.py
@@ -1,37 +1,65 @@
 # Unit Converter
 #
 #   handles temp, currency, volume, mass, etc
+#   for now, it rounds to 3rd decimal #
 #
 #   to-do list:
+#     [X] temp:
 #       [X] celsius -> fahrenheit
-#       [X] fahrenheit -> celsius
+#       [X] fahrenheit -> Celsius
+#       [X] Fahrenheit -> Kelvin
+#       [X] Kelvin -> Fahrenheit
+#       [X] Celsius -> Kelvin
+#       [X] Kelvin -> Celsius
+#     [] mass:
 #       [X] lb -> kg
 #       [X] kg -> lb
 
-def uconv(u_in, u_out, value):
+def tempconv(u_in, u_out, value):
     u_in = u_in.lower()
     u_out = u_out.lower()
-
-    if u_in == 'celsius' or 'c' in u_in and u_out == 'fahrenheit' or 'f' in u_out:
-        value_conv = value * 1.8 + 32
-
-    elif u_in == 'fahrenheit' or 'f' in u_in and u_out == 'celsius' or 'c' in u_out:
-        value_conv = (value - 32) / 1.8
-
-    elif u_in == 'kilograms' or 'kg' in u_in and u_out == 'pounds' or 'lb' in u_out:
-        value_conv = value * 2.2046
-
-    elif u_in == 'pounds' or 'lb' in u_in and u_out == 'kilograms' or 'kg' in u_out:
-        value_conv = value / 2.2046
     
+    if u_in == 'c' and u_out == 'f':
+        value_conv = value * 1.8 + 32
+        u_in = 'C'
+        u_out = 'F'
+    
+    elif u_in == 'f'and u_out == 'c':
+        value_conv = (value - 32) / 1.8
+        u_in = 'F'
+        u_out = 'C'
+    
+    elif u_in == 'c' and u_out == 'k':
+        value_conv = value + 273.15
+        u_in = 'C'
+        u_out = 'K'
+        
+    elif u_in == 'k' and u_out == 'c':
+        value_conv = value - 273.15
+        u_in = 'K'
+        u_out = 'C'
+
+    elif u_in == 'f' and u_out == 'k':
+        value_conv = ((value - 32) / 1.8) + 273.15
+        u_in = 'F'
+        u_out = 'K'
+        
+    elif u_in == 'k' and u_out == 'f':
+        value_conv = (value - 273.15) * 1.8 + 32        
+        u_in = 'K'
+        u_out = 'F'
+        
     else:
         return ("Not valid input/output conversion values")
     
-    return str(value) + " " + u_in + " --> " + str(value_conv) + " " + u_out
+    return str(value) + " " + u_in + " --> " + str(round(value_conv, 3)) + " " + u_out
 
-# Test cases
-#print (uconv('Celsius', 'Fahrenheit', 47.5))
-#print (uconv('Fahrenheit', 'Celsius', 3))
-#print (uconv('f','c',3))
-#print (uconv('kg', 'pounds', 5))
-#print (uconv('lb', 'kilograms', 5))
+
+#def massconv(u_in, u_out, value):
+#    if u_in == 'kilograms' or 'kg' in u_in and u_out == 'pounds' or 'lb' in u_out:
+#        value_conv = value * 2.2046
+#
+#    elif u_in == 'pounds' or 'lb' in u_in and u_out == 'kilograms' or 'kg' in u_out:
+#        value_conv = value / 2.2046
+print (tempconv('f', 'k', 84.90))
+
