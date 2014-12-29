be27dc161db51b0845b4f6ef3dd6c8cecd44fc33
Jeff Bahns <jeffbahns@gmail.com>
2014-12-29 14:44:20 -0800

    api testing

--- weatherfinder.py
+++ weatherfinder.py
@@ -4,9 +4,11 @@ import requests, re
 from bs4 import BeautifulSoup
 
 def weatherfind():
-    url = "http://www.weather.com/weather/today/l/94513"
-    r = requests.get(url)
-    soup = BeautifulSoup(r.content)
+    #url = "http://www.weather.com/weather/today/l/94513"
+    #r = requests.get(url)
+    #soup = BeautifulSoup(r.content)
+    req = requests.get('http://dsx.weather.com/wxd/v2/MORecord/en_US/(USCA0478:1:US)?api=7bb1c920-7027-4289-9c96-ae5e263980bc')
+    data = req.json()
+    print(data[0]['doc']['MOData']['tmpF'])
     
-    print (temps)
 (weatherfind())
