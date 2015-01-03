24ba57e91ae1580a546b57e3032687544bb6924f
Jeff Bahns <jeffbahns@gmail.com>
2015-01-02 19:39:17 -0800

    IP

--- gradebook.py
+++ gradebook.py
@@ -1,8 +1,36 @@
 # Grade Manager
+import os
 
+def gradebook(book):
+    os.system('clear')
+    print ("""
+________________________________________________________________________\n
+Hello, Welcome to "Grade Book"\n
+Choose from the following actions to perform\n\n
+ 1 : Print GPA\n 2 : Print Total Units\n 3 : Print Full Course History
+ 4 : Print Total Classes\n 5 : Print Grade Tallies\n (Enter "Exit" to quit program
+________________________________________________________________________
+""")
+    while True:
+        inp = input(">>  ")
+        if (inp == '1'):
+            book.print_gpa()
+        elif (inp == '2'):
+            pass
+        elif (inp == '3'):
+            pass
+        elif (inp == '4'):
+            pass
+        elif (inp == '5'):
+            pass
+        elif (inp.lower() == "exit"):
+            break
+        else:
+            print ("ERROR: Not a valid command")
+    print ("Goodbye!")
+    
 class Grade():
 
-    
     def __init__(self, mark, units, course, teacher):
         self.mark = mark
         self.units = units
@@ -14,7 +42,7 @@ class GradeBook():
     def __init__(self, grade_list):
         self.grade_list = grade_list
         self.gpa = 0
-        self.units = 
+        self.units = 0
         self.values = { 'A' : 4,
                         'B' : 3,
                         'C' : 2,
@@ -27,15 +55,16 @@ class GradeBook():
         totalp = 0
         totalu = 0
         for g in self.grade_list:
-            totalp += self.values[g.mark] * g.units
-            totalu += g.units
-        self.gpa = totalp / totalu
-        self.units = totalu 
-        print (self.gpa)
+            #totalp += self.values[g.mark] * g.units
+            #totalu += g.units
+            print (g.units)
+        #self.gpa = totalp / totalu 
+        #print (self.gpa)
 
     def print_grades(self):
         for g in self.grade_list:
             print (g.mark, g.units, g.course, g.teacher)
+
 
 COMSC_122 = Grade('A', 3, 'COMSC-132', 'Giambattista'),
 RA_015 = Grade('A', 3, 'RA-015', ''),
@@ -61,13 +90,31 @@ MATH_070 = Grade('A', 4, 'MATH-070', 'Stricker'),
 COMSC_142 = Grade('A', 3, 'COMSC-142', 'Giambattista'),
 COMSC_122 = Grade('A', 3, 'COMSC-122', 'Giambattista')
 
-COURSES = [ COMSC122, RA_015, RA_012, RA_010, 
-            MUSIC_093, MUSIC_015, RA_025, RA_021, 
-            RA_020, ENGL_100, MATH_040, SPCH_110, 
-            PSYCH_014, MATH_050, COMSC_044, BIOSC_010,
-            PHYS_040, PHIL_041, MATH_060, DRAMA_015,
-            MATH_070, COMSC_142, COMSC_122 ]
+courses = [ COMSC_122,
+            RA_015,
+            RA_012,
+            RA_010, 
+            MUSIC_093,
+            MUSIC_015,
+            RA_025,
+            RA_021, 
+            RA_020,
+            ENGL_100,
+            MATH_040,
+            SPCH_110, 
+            PSYCH_014,
+            MATH_050,
+            COMSC_044,
+            BIOSC_010,
+            PHYS_040,
+            PHIL_041,
+            MATH_060,
+            DRAMA_015,
+            MATH_070,
+            COMSC_142,
+            COMSC_122
+            ]
 
-gbook = GradeBook([])
-for course in courses:
-    gbook.add(course)
+gbook = GradeBook([COMSC_122, RA_015])
+print (courses[3].mark)
+#gradebook(gbook)
