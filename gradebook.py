2c7016daf3aca80ca92c214cc5ab4978b41cad1d
Jeff Bahns <jeffbahns@gmail.com>
2015-01-02 16:05:30 -0800

    Update gradebook.py

--- gradebook.py
+++ gradebook.py
@@ -37,35 +37,37 @@ class GradeBook():
         for g in self.grade_list:
             print (g.mark, g.units, g.course, g.teacher)
 
-COURSES = [ comsc_122 = Grade('A', 3, 'COMSC-132', 'Giambattista'),
-            ra_015 = Grade('A', 3, 'RA-015', ''),
-            ra_012 = Grade('B', 3, 'RA-012', ''),
-            ra_010 = Grade('A', 3, 'RA-010', ''),
-            MUSIC_093 = Grade('A', 2, 'MUSIC-093', ''),
-            MUSIC_015 = Grade('A', 3, 'MUSIC-015', ''),
-            RA_025 = Grade('A', 3, 'RA-025', ''),
-            RA_021 = Grade('A', 2, 'RA-021', ''),
-            RA_020 = Grade('A', 3, 'RA-020', ''),
-            ENGL_100 = Grade('A', 3, 'ENGL-100', ''),
-            MATH_040 = Grade('A', 4, 'MATH-040', 'Hubbard'),
-            SPCH_110 = Grade('A', 3, 'SPCH-110', 'Petersen'),
-            PSYCH_014 = Grade('B', 3, 'PSYCH-014', ''),
-            MATH_050 = Grade('B', 4, 'MATH-050', 'Horne'),
-            COMSC_044 = Grade('A', 3, 'COMSC-044', 'Giambattista'),
-            BIOSC_010 = Grade('A', 4, 'BIOSC-010', ''),
-            PHYS_040 = Grade('C', 4, 'PHYS-040', 'Stone'),
-            PHIL_041 = Grade('A', 3, 'PHIL-041', ''),
-            MATH_060 = Grade('B', 4, 'MATH-060', ''),
-            DRAMA_015 = Grade('A', 3, 'DRAMA-015', 'Norris'),
-            MATH_070 = Grade('A', 4, 'MATH-070', 'Stricker'),
-            COMSC_142 = Grade('A', 3, 'COMSC-142', 'Giambattista'),
-            COMSC_122 = Grade('A', 3, 'COMSC-122', 'Giambattista')
-            ]
+COMSC_122 = Grade('A', 3, 'COMSC-132', 'Giambattista'),
+RA_015 = Grade('A', 3, 'RA-015', ''),
+RA_012 = Grade('B', 3, 'RA-012', ''),
+RA_010 = Grade('A', 3, 'RA-010', ''),
+MUSIC_093 = Grade('A', 2, 'MUSIC-093', ''),
+MUSIC_015 = Grade('A', 3, 'MUSIC-015', ''),
+RA_025 = Grade('A', 3, 'RA-025', ''),
+RA_021 = Grade('A', 2, 'RA-021', ''),
+RA_020 = Grade('A', 3, 'RA-020', ''),
+ENGL_100 = Grade('A', 3, 'ENGL-100', ''),
+MATH_040 = Grade('A', 4, 'MATH-040', 'Hubbard'),
+SPCH_110 = Grade('A', 3, 'SPCH-110', 'Petersen'),
+PSYCH_014 = Grade('B', 3, 'PSYCH-014', ''),
+MATH_050 = Grade('B', 4, 'MATH-050', 'Horne'),
+COMSC_044 = Grade('A', 3, 'COMSC-044', 'Giambattista'),
+BIOSC_010 = Grade('A', 4, 'BIOSC-010', ''),
+PHYS_040 = Grade('C', 4, 'PHYS-040', 'Stone'),
+PHIL_041 = Grade('A', 3, 'PHIL-041', ''),
+MATH_060 = Grade('B', 4, 'MATH-060', ''),
+DRAMA_015 = Grade('A', 3, 'DRAMA-015', 'Norris'),
+MATH_070 = Grade('A', 4, 'MATH-070', 'Stricker'),
+COMSC_142 = Grade('A', 3, 'COMSC-142', 'Giambattista'),
+COMSC_122 = Grade('A', 3, 'COMSC-122', 'Giambattista')
+
+COURSES = [ COMSC122, RA_015, RA_012, RA_010, 
+            MUSIC_093, MUSIC_015, RA_025, RA_021, 
+            RA_020, ENGL_100, MATH_040, SPCH_110, 
+            PSYCH_014, MATH_050, COMSC_044, BIOSC_010,
+            PHYS_040, PHIL_041, MATH_060, DRAMA_015,
+            MATH_070, COMSC_142, COMSC_122 ]
 
 gbook = GradeBook([])
-gbook.add(comsc_122)
-
-
-gbook.print_grades()
-gbook.print_gpa()
-print (gbook.gpa)
+for course in courses:
+    gbook.add(course)
