fdcc997389a028420b53a4d364cc58d639aa295c
Jeff Bahns <jeffbahns@gmail.com>
2015-02-09 21:28:06 -0800

    at a functional level, gui develop soon

--- weatherfinder.py
+++ weatherfinder.py
@@ -1,14 +1,32 @@
-# Weather Finder
+# weatherfind
 
-import requests, re
+import requests, re, os
 from bs4 import BeautifulSoup
+from json import load
+s='1868967083e9'
+s2='ca9a'
+url = "http://api.wunderground.com/api/{}/conditions/q/{}/{}.json"
+apiKey = "xxxxxxxxxxxx"
 
-def weatherfind():
-    #url = "http://www.weather.com/weather/today/l/94513"
-    #r = requests.get(url)
-    #soup = BeautifulSoup(r.content)
-    req = requests.get('http://dsx.weather.com/wxd/v2/MORecord/en_US/(USCA0478:1:US)?api=7bb1c920-7027-4289-9c96-ae5e263980bc')
-    data = req.json()
-    print(data[0]['doc']['MOData']['tmpF'])
+def weatherfind(city, state):
+	city = re.sub("[^a-zA-Z]", "_", city)
+	r = requests.get(url.format(apiKey, state,city))
+	data = r.json()
+	co = data['current_observation']
     
-(weatherfind())
+    # grabs all info from json file and formats it onto screen
+	print ("____________________________________________________")
+	print (co['local_time_rfc822'])
+	print ("Forecast for " + co['display_location']['full'])
+	print ("---------------------------------------------")
+	print ("Weather : " + co['weather'])
+	print ("Current Temperature : " + str(co['temp_f']) + "F")
+	print ("Feels like : " + str(co['feelslike_f']) + "F")
+	print ("Wind : " + str(co['wind_mph']) + "MPH")
+	print ("____________________________________________________|")
+
+
+os.system('clear')
+testcity = input("Enter a city name:\n>>  ")
+teststate = input("Enter a state:\n>>  ")
+(weatherfind(testcity, teststate))
